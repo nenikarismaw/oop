424aa3deabbb61f35d9ffd35d342ca8ccb5ae55f
Neni Karismawati <karismawatineni@gmail.com>
2020-07-30 16:45:13 +0700

    oop

---
+++ index.php
@@ -0,0 +1,11 @@
+$sheep = new Animal("shaun");
+
+echo $sheep->name; // "shaun"
+echo $sheep->legs; // 2
+echo $sheep->cold_blooded // false
+
+$sungokong = new Ape("kera sakti");
+$sungokong->yell() // "Auooo"
+
+$kodok = new Frog("buduk");
+$kodok->jump() ; // "hop hop"
